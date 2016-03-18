8d299a46988741b2714d5a2d10dc810fd9da3ba4
Percy Yeung <percyyeung2002@gmail.com>
2016-03-18 17:28:51 -0400

    curl post?

--- library/groupme_functions.php
+++ library/groupme_functions.php
@@ -6,15 +6,17 @@ function gmImageService($url) {
     $gmISURL = 'https://image.groupme.com/pictures?access_token='.$GLOBALS['AUTH_TOKEN'];
     $raw = curlGrabRawBinary($url);
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $gmISURL);
-    // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data'));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $raw);
-    logger('Response: '. $response = curl_exec($ch));
-    curl_close($ch);
-    return $response;
+    // $ch = curl_init();
+    // curl_setopt($ch, CURLOPT_URL, $gmISURL);
+    // // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data'));
+    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // curl_setopt($ch, CURLOPT_POST, 1);
+    // curl_setopt($ch, CURLOPT_POSTFIELDS, $raw);
+    // logger('Response: '. $response = curl_exec($ch));
+    // curl_close($ch);
+    // return $response;
 
-    // return sendPostJson($gmISURL, $raw, "multipart/form-data");
+    $postfields = array('file'=>$raw);
+
+    return sendPostJson($gmISURL, $postfields);
 }
