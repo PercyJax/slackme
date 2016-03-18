7dce50efb82a0e0a00347a2fb3bc77b5d25c1e44
Percy Yeung <percyyeung2002@gmail.com>
2016-03-18 16:40:34 -0400

    auth token global

--- library/groupme_functions.php
+++ library/groupme_functions.php
@@ -3,6 +3,6 @@
 require_once ROOT_DIR.'/groupme_config.php';
 
 function gmImageService($url) {
-    $gmISURL = 'https://image.groupme.com/pictures?access_token='.$AUTH_TOKEN;
+    $gmISURL = 'https://image.groupme.com/pictures?access_token='.$GLOBALS['AUTH_TOKEN'];
     return sendPostJson($gmISURL, "file=".$url, "multipart/form-data");
 }
