a24104798fafa01b750f3a0f8f769ad9de681201
Percy Yeung <percyyeung2002@gmail.com>
2016-03-18 17:39:53 -0400

    no header?

--- library/groupme_functions.php
+++ library/groupme_functions.php
@@ -6,17 +6,19 @@ function gmImageService($url) {
     $gmISURL = 'https://image.groupme.com/pictures?access_token='.$GLOBALS['AUTH_TOKEN'];
     $raw = curlGrabRawBinary($url);
 
-    // $ch = curl_init();
-    // curl_setopt($ch, CURLOPT_URL, $gmISURL);
-    // // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data'));
-    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    // curl_setopt($ch, CURLOPT_POST, 1);
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, $raw);
-    // logger('Response: '. $response = curl_exec($ch));
-    // curl_close($ch);
-    // return $response;
-
     $postfields = array('file'=>base64_encode($raw));
 
-    return sendPostJson($gmISURL, json_encode($postfields))->payload->url;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $gmISURL);
+    // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+    logger('Response: '. $response = curl_exec($ch));
+    curl_close($ch);
+    return json_decode($response)->payload->url;
+
+    
+
+    // return json_decode(sendPostJson($gmISURL, json_encode($postfields)))->payload->url;
 }
