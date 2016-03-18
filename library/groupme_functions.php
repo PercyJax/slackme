410d0586834450f8fa2ce56b9cf2c43a1f81cb0d
Percy Yeung <percyyeung2002@gmail.com>
2016-03-18 16:33:40 -0400

    semicolon

--- library/groupme_functions.php
+++ library/groupme_functions.php
@@ -4,5 +4,5 @@ require_once ROOT_DIR.'/groupme_config.php';
 
 function gmImageService($url) {
     $gmISURL = 'https://image.groupme.com/pictures?access_token='.$AUTH_TOKEN;
-    return sendPostJson($gmISURL, "file=".$url, "multipart/form-data")
+    return sendPostJson($gmISURL, "file=".$url, "multipart/form-data");
 }
